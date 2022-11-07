24b3d50544b901d1a10f92d6eddea34a49ed4cee
steffenskov <steffen@ckode.dk>
2022-11-07 20:44:59 +0100

    code simplification

--- src/StrongTypedId/Converters/StrongTypedIdTypeConverter.cs
+++ src/StrongTypedId/Converters/StrongTypedIdTypeConverter.cs
@@ -19,7 +19,7 @@ namespace StrongTypedId.Converters
 			return sourceType == typeof(string) || base.CanConvertFrom(context, sourceType);
 		}
 
-		public override object? ConvertFrom(ITypeDescriptorContext? context, CultureInfo? culture, object value)
+		public override object? ConvertFrom(ITypeDescriptorContext context, CultureInfo culture, object value)
 		{
 			var stringValue = value as string;
 			if (!string.IsNullOrEmpty(stringValue) && TryParse(stringValue, out var primitiveId))
@@ -34,19 +34,19 @@ namespace StrongTypedId.Converters
 		{
 			primitiveId = typeof(TPrimitiveId) switch
 			{
-				Type t when t == typeof(bool) => bool.Parse(stringValue),
-				Type t when t == typeof(Guid) => Guid.Parse(stringValue),
-				Type t when t == typeof(short) => short.Parse(stringValue),
-				Type t when t == typeof(int) => int.Parse(stringValue),
-				Type t when t == typeof(long) => long.Parse(stringValue),
-				Type t when t == typeof(ushort) => ushort.Parse(stringValue),
-				Type t when t == typeof(uint) => uint.Parse(stringValue),
-				Type t when t == typeof(ulong) => ulong.Parse(stringValue),
-				Type t when t == typeof(float) => float.Parse(stringValue),
-				Type t when t == typeof(double) => double.Parse(stringValue),
-				Type t when t == typeof(decimal) => decimal.Parse(stringValue),
-				Type t when t == typeof(byte) => byte.Parse(stringValue),
-				Type t when t == typeof(sbyte) => sbyte.Parse(stringValue),
+				{ } t when t == typeof(bool) => bool.Parse(stringValue),
+				{ } t when t == typeof(Guid) => Guid.Parse(stringValue),
+				{ } t when t == typeof(short) => short.Parse(stringValue),
+				{ } t when t == typeof(int) => int.Parse(stringValue),
+				{ } t when t == typeof(long) => long.Parse(stringValue),
+				{ } t when t == typeof(ushort) => ushort.Parse(stringValue),
+				{ } t when t == typeof(uint) => uint.Parse(stringValue),
+				{ } t when t == typeof(ulong) => ulong.Parse(stringValue),
+				{ } t when t == typeof(float) => float.Parse(stringValue),
+				{ } t when t == typeof(double) => double.Parse(stringValue),
+				{ } t when t == typeof(decimal) => decimal.Parse(stringValue),
+				{ } t when t == typeof(byte) => byte.Parse(stringValue),
+				{ } t when t == typeof(sbyte) => sbyte.Parse(stringValue),
 				_ => throw new NotSupportedException()
 			};
 			return true;
--- src/StrongTypedId/Converters/SystemTextJsonConverter.cs
+++ src/StrongTypedId/Converters/SystemTextJsonConverter.cs
@@ -23,19 +23,19 @@ namespace StrongTypedId.Converters
 		{
 			return typeof(TPrimitiveId) switch
 			{
-				Type t when t == typeof(bool) => reader.GetBoolean(),
-				Type t when t == typeof(Guid) => reader.GetGuid(),
-				Type t when t == typeof(short) => reader.GetInt16(),
-				Type t when t == typeof(int) => reader.GetInt32(),
-				Type t when t == typeof(long) => reader.GetInt64(),
-				Type t when t == typeof(ushort) => reader.GetUInt16(),
-				Type t when t == typeof(uint) => reader.GetUInt32(),
-				Type t when t == typeof(ulong) => reader.GetUInt64(),
-				Type t when t == typeof(float) => reader.GetSingle(),
-				Type t when t == typeof(double) => reader.GetDouble(),
-				Type t when t == typeof(decimal) => reader.GetDecimal(),
-				Type t when t == typeof(byte) => reader.GetByte(),
-				Type t when t == typeof(sbyte) => reader.GetSByte(),
+				{ } t when t == typeof(bool) => reader.GetBoolean(),
+				{ } t when t == typeof(Guid) => reader.GetGuid(),
+				{ } t when t == typeof(short) => reader.GetInt16(),
+				{ } t when t == typeof(int) => reader.GetInt32(),
+				{ } t when t == typeof(long) => reader.GetInt64(),
+				{ } t when t == typeof(ushort) => reader.GetUInt16(),
+				{ } t when t == typeof(uint) => reader.GetUInt32(),
+				{ } t when t == typeof(ulong) => reader.GetUInt64(),
+				{ } t when t == typeof(float) => reader.GetSingle(),
+				{ } t when t == typeof(double) => reader.GetDouble(),
+				{ } t when t == typeof(decimal) => reader.GetDecimal(),
+				{ } t when t == typeof(byte) => reader.GetByte(),
+				{ } t when t == typeof(sbyte) => reader.GetSByte(),
 				_ => throw new NotSupportedException()
 			};
 		}
