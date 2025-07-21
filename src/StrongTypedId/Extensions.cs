d6cb9d17499559421802790911d1309af033eb4a
steffenskov <steffen@ckode.dk>
2025-07-21 14:41:17 +0200

    fix: Fixed Type name in exception thrown by TypeConverter registrator

--- src/StrongTypedId/Extensions.cs
+++ src/StrongTypedId/Extensions.cs
@@ -49,35 +49,40 @@ public static class Extensions
 	}
 
 	/// <summary>
-	///     Gets the StrongTypedValue type specification for a given type, should not be called unless IsStrongTypedValue has
-	///     been checked first.
-	/// </summary>
-	public static Type GetStrongTypedValueType(this Type type)
-	{
-		if (type is { IsAbstract: true, IsGenericType: true } && type.GetGenericTypeDefinition() == typeof(StrongTypedValue<,>))
-		{
-			return type;
-		}
-
-		if (type.BaseType is not null)
-		{
-			return GetStrongTypedValueType(type.BaseType);
-		}
-
-		throw new InvalidOperationException($"Type {type.Name} does not inherit StrongTypedValue<,>");
-	}
-
-	/// <summary>
 	///     Gets the generic type arguments to a StrongTypedValue type, should not be called unless IsStrongTypedValue has
 	///     been checked first.
 	/// </summary>
 	public static (Type TSelf, Type TPrimitive) GetStrongTypedValueArguments(this Type type)
 	{
-		var strongTypedValueType = type.GetStrongTypedValueType();
+		var strongTypedValueType = GetStrongTypedValueType(type);
 		var arguments = strongTypedValueType.GetGenericArguments();
 		var tself = arguments[0];
 		var tprimitive = arguments[1];
 
 		return (tself, tprimitive);
+	}
+
+	/// <summary>
+	///     Gets the StrongTypedValue type specification for a given type, should not be called unless IsStrongTypedValue has
+	///     been checked first.
+	/// </summary>
+	private static Type GetStrongTypedValueType(Type type)
+	{
+		var initialType = type;
+		while (true)
+		{
+			if (type is { IsAbstract: true, IsGenericType: true } && type.GetGenericTypeDefinition() == typeof(StrongTypedValue<,>))
+			{
+				return type;
+			}
+
+			if (type.BaseType is not null)
+			{
+				type = type.BaseType;
+				continue;
+			}
+
+			throw new InvalidOperationException($"Type {initialType.Name} does not inherit StrongTypedValue<,>");
+		}
 	}
 }
