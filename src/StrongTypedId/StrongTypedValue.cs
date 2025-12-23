a03bbc95f4dd4fb1d6beb3c6ab177fcbd901d813
steffenskov <steffen@ckode.dk>
2025-12-23 11:15:29 +0100

    WIP

--- src/StrongTypedId/StrongTypedValue.cs
+++ src/StrongTypedId/StrongTypedValue.cs
@@ -21,6 +21,106 @@ public abstract class StrongTypedValue<TSelf, TPrimitiveValue> : StrongTypedValu
 
 	public TPrimitiveValue PrimitiveValue { get; }
 
+	public static bool operator ==(StrongTypedValue<TSelf, TPrimitiveValue>? a, StrongTypedValue<TSelf, TPrimitiveValue>? b)
+	{
+		if (a is null && b is null)
+		{
+			return true;
+		}
+
+		return a?.PrimitiveValue.Equals(b is null ? null : b.PrimitiveValue) == true;
+	}
+
+	public static bool operator ==(TSelf? a, TPrimitiveValue b)
+	{
+		return a?.PrimitiveValue.Equals(b) == true;
+	}
+
+	public static bool operator ==(TPrimitiveValue? a, TSelf? b)
+	{
+		return a?.Equals(b is null ? null : b.PrimitiveValue) == true;
+	}
+
+	public static bool operator >(TSelf? a, IStrongTypedValue<TSelf, TPrimitiveValue>? b)
+	{
+		return a?.PrimitiveValue.CompareTo(b is null ? null : b.PrimitiveValue) > 0;
+	}
+
+	public static bool operator >(TSelf? a, TPrimitiveValue? b)
+	{
+		return a?.PrimitiveValue.CompareTo(b) > 0;
+	}
+
+	public static bool operator >(TPrimitiveValue? a, TSelf? b)
+	{
+		return a?.CompareTo(b is null ? null : b.PrimitiveValue) > 0;
+	}
+
+	public static bool operator <(TSelf? a, IStrongTypedValue<TSelf, TPrimitiveValue>? b)
+	{
+		return a?.PrimitiveValue.CompareTo(b is null ? null : b.PrimitiveValue) < 0;
+	}
+
+	public static bool operator <(TSelf? a, TPrimitiveValue? b)
+	{
+		return a?.PrimitiveValue.CompareTo(b) < 0;
+	}
+
+	public static bool operator <(TPrimitiveValue? a, TSelf? b)
+	{
+		return a?.CompareTo(b is null ? null : b.PrimitiveValue) < 0;
+	}
+
+	public static bool operator >=(TSelf? a, IStrongTypedValue<TSelf, TPrimitiveValue>? b)
+	{
+		return a?.PrimitiveValue.CompareTo(b is null ? null : b.PrimitiveValue) >= 0;
+	}
+
+	public static bool operator >=(TSelf? a, TPrimitiveValue? b)
+	{
+		return a?.PrimitiveValue.CompareTo(b) >= 0;
+	}
+
+	public static bool operator >=(TPrimitiveValue? a, TSelf? b)
+	{
+		return a?.CompareTo(b is null ? null : b.PrimitiveValue) >= 0;
+	}
+
+	public static bool operator <=(TSelf? a, IStrongTypedValue<TSelf, TPrimitiveValue>? b)
+	{
+		return a?.PrimitiveValue.CompareTo(b is null ? null : b.PrimitiveValue) <= 0;
+	}
+
+	public static bool operator <=(TSelf? a, TPrimitiveValue? b)
+	{
+		return a?.PrimitiveValue.CompareTo(b) <= 0;
+	}
+
+	public static bool operator <=(TPrimitiveValue? a, TSelf? b)
+	{
+		return a?.CompareTo(b is null ? null : b.PrimitiveValue) <= 0;
+	}
+
+	public static bool operator !=(TSelf? a, IStrongTypedValue<TSelf, TPrimitiveValue>? b)
+	{
+		if (a is null && b is null)
+		{
+			return false;
+		}
+
+		return a?.PrimitiveValue.Equals(b is null ? null : b.PrimitiveValue) != true;
+	}
+
+	public static bool operator !=(TSelf? a, TPrimitiveValue b)
+	{
+		return a?.PrimitiveValue.Equals(b) != true;
+	}
+
+	public static bool operator !=(TPrimitiveValue? a, TSelf? b)
+	{
+		return a?.Equals(b is null ? null : b.PrimitiveValue) != true;
+	}
+
 	public override bool Equals(object? obj)
 	{
 		if (obj is TSelf strongTyped)
